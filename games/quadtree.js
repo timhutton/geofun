53bdef2efd587c1e0d3974e1db6bb02c31f81964
Tim Hutton <tim.hutton@gmail.com>
2017-06-16 23:22:08 +0100

    Keeping track of when points were added allows to shortcut queries if nothing has changed. Allowing rectangular regions.

--- games/quadtree.js
+++ games/quadtree.js
@@ -3,31 +3,33 @@ function XY(x, y) {
     this.y = y;
 }
 
-function AABB(center, radius) {
+function AABB(center, x_radius, y_radius) {
     this.center = center;
-    this.radius = radius;
+    this.x_radius = x_radius;
+    this.y_radius = y_radius;
 }
 
 AABB.prototype.containsPoint = function(p) {
-    return p.x >= this.center.x - this.radius
-        && p.x <= this.center.x + this.radius
-        && p.y >= this.center.y - this.radius
-        && p.y <= this.center.y + this.radius;
+    return ( p.x >= this.center.x - this.x_radius )
+        && ( p.x <= this.center.x + this.x_radius )
+        && ( p.y >= this.center.y - this.y_radius )
+        && ( p.y <= this.center.y + this.y_radius );
 }
 
 AABB.prototype.intersectsAABB = function(other) {
-    return (Math.abs(other.center.x - this.center.x) < (other.radius + this.radius)) &&
-           (Math.abs(other.center.y - this.center.y) < (other.radius + this.radius));
+    return (Math.abs(other.center.x - this.center.x) < (other.x_radius + this.x_radius)) &&
+           (Math.abs(other.center.y - this.center.y) < (other.y_radius + this.y_radius));
 }
 
 function QuadTree(boundary) {
     this.boundary = boundary;
     this.MAX_POINTS = 10;
-    this.points=[];
-    this.children=[]; // empty or size 4: NW, NE, SW, SE
+    this.points = [];
+    this.children = []; // empty or size 4: NW, NE, SW, SE
+    this.updated = 0; // milliseconds since 1970
 }
 
-// insert any object that has a member p of type XY
+// obj has at least: { XY p; int updated; }
 QuadTree.prototype.insert = function(obj) {
     if(!this.boundary.containsPoint(obj.p)) {
         return false; // point is outside our region
@@ -35,6 +37,8 @@ QuadTree.prototype.insert = function(obj) {
     if(this.points.length<this.MAX_POINTS) {
         // no need to subdivide, just store it
         this.points.push(obj);
+        // keep track of when any point in this quad was updated
+        this.updated = Math.max(this.updated, obj.updated);
         return true;
     }
     if(this.children.length==0) {
@@ -42,6 +46,8 @@ QuadTree.prototype.insert = function(obj) {
     }
     for(var i=0;i<4;i++) {
         if(this.children[i].insert(obj)) {
+            // keep track of when any point in any child quad was updated
+            this.updated = Math.max(this.updated, obj.updated);
             return true;
         }
     }
@@ -49,29 +55,36 @@ QuadTree.prototype.insert = function(obj) {
 }
 
 QuadTree.prototype.subdivide = function() {
-    var r = this.boundary.radius / 2;
-    var centers = [ new XY(this.boundary.center.x-r,this.boundary.center.y-r),
-                    new XY(this.boundary.center.x+r,this.boundary.center.y-r),
-                    new XY(this.boundary.center.x-r,this.boundary.center.y+r),
-                    new XY(this.boundary.center.x+r,this.boundary.center.y+r) ];
+    var xr = this.boundary.x_radius / 2;
+    var yr = this.boundary.y_radius / 2;
+    var centers = [ new XY(this.boundary.center.x-xr,this.boundary.center.y-yr),
+                    new XY(this.boundary.center.x+xr,this.boundary.center.y-yr),
+                    new XY(this.boundary.center.x-xr,this.boundary.center.y+yr),
+                    new XY(this.boundary.center.x+xr,this.boundary.center.y+yr) ];
     for(var i=0;i<centers.length;i++) {
-        this.children.push(new QuadTree(new AABB(centers[i],r)));
+        this.children.push(new QuadTree(new AABB(centers[i],xr,yr)));
     }
 }
 
-QuadTree.prototype.queryRange = function(boundary) {
+// 'boundary' is AABB, 'since' is int containing milliseconds since 1970
+QuadTree.prototype.queryAreaSince = function(boundary,since) {
     var pts = [];
-
+    if(this.updated < since) {
+        // nothing in this quad has been updated since the time specified
+        return pts;
+    }
     if(!this.boundary.intersectsAABB(boundary)) {
+        // nothing in this quad is inside the area specified
         return pts;
     }
     for(var i = 0; i < this.points.length; i++) {
-        if(boundary.containsPoint(this.points[i].p)) {
-            pts.push(this.points[i]);
+        var obj = this.points[i];
+        if(obj.updated > since && boundary.containsPoint(obj.p)) {
+            pts.push(obj);
         }
     }
     for(var i = 0; i < this.children.length; i++) {
-        pts.push(...this.children[i].queryRange(boundary));
+        pts.push(...this.children[i].queryAreaSince(boundary,since));
     }
     return pts;
 }
