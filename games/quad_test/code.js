e14a1cde01479e8f4739e2439caaa870bdf370aa
Tim Hutton <tim.hutton@gmail.com>
2017-06-19 22:23:13 +0100

    Added demo of center tile location and size, using new coordinate system.

--- games/quad_test/code.js
+++ games/quad_test/code.js
@@ -3,7 +3,7 @@
 // TODO: Use Point coordinates for square tiles. Store in quadtree as integers to avoid ambiguity. Replace tiles when updated.
 
 // TODO: Have two quadtrees one as a server, the other as a client, updating itself. Check
-// that can get reasonable updating behavior with low bandwidth even when the server has 
+// that can get reasonable updating behavior with low bandwidth even when the server has
 // many tiles. Mark higher level quads as checked but too many tiles to update.
 
 // TODO: As above but cache locally to save on startup time.
@@ -21,12 +21,40 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
     id: 'mapbox.light'
     }).addTo(map);
+map.on('dragend', updateTileOutline );
+
 
 addButton("\u272A", "add random points", addRandomPoints, map);
-var x_range = 20026376.39;
-var y_range = 20048966.10;
+var x_range = 20026376.39; // tiles with centers at 324n units in this coordinate system, with n in [-61809, 61809]
+var y_range = 20048966.10; // likewise with n in [-61879, 61879]
 var quadtree = new QuadTree(new AABB(new XY(0,0),x_range,y_range));
 var quad_layers = []
+
+function latLngToTileIndices(latlng) {
+    var divisor = 324;
+    var p = L.Projection.SphericalMercator.project(latlng);
+    return { x: Math.round(p.x/divisor), y: Math.round(p.y/divisor) };
+}
+
+function tileIndicesToLatLng(p) {
+    var divisor = 324;
+    return L.Projection.SphericalMercator.unproject(new L.Point(p.x*divisor,p.y*divisor));
+}
+
+function getTileBounds(loc) {
+    var tc = latLngToTileIndices(loc);
+    var p1 = tileIndicesToLatLng(new L.Point(tc.x-0.5,tc.y-0.5));
+    var p2 = tileIndicesToLatLng(new L.Point(tc.x+0.5,tc.y+0.5));
+    return [p1,p2];
+}
+
+function updateTileOutline() {
+    if(typeof current_tile_outline !== 'undefined') {
+        map.removeLayer(current_tile_outline);
+    }
+    current_tile_outline = L.rectangle(getTileBounds(map.getCenter()), {color: "#000000", weight: 1, fill: false});
+    current_tile_outline.addTo(map);
+}
 
 function eraseQuads() {
     for(var i=0;i<quad_layers.length;i++) {
