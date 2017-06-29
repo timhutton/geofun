240b4cf78eb65ff2d6bfa4862c33e6bd8bb6616b
Tim Hutton <tim.hutton@gmail.com>
2017-06-29 17:44:20 +0100

    First implementation of UpdateLog. Untested.

--- games/UpdateLog.js
+++ games/UpdateLog.js
@@ -1,23 +1,44 @@
 function UpdateLog() {
-    this.time_slots = [100,1000,10000,100000,1000000]; // 100s to 1Ms=11days
-    this.last_event = [0,0,0,0,0];
-    this.n_since = [0,0,0,0,0];
+    this.time_slots = [100,1000,10000,100000,1000000]; // N time slots: each is emptied at intervals ranging from 100s to 1Ms=11days
+    this.last_emptied = [0,0,0,0,0]; // N: time in seconds since 1970-01-01 of the last time this slot was emptied
+    this.n_since = [0,0,0,0,0,0]; // N+1: count of events in this slot
 }
 
 UpdateLog.prototype.add = function() {
-    var time_now = new Date().getTime();
-    // add the event at time time_now into the log
+    // add an event to the log
+    var time_now = getTime();
+    this.refreshSlots(time_now);
+    this.n_since[0]++;
 }
 
 UpdateLog.prototype.moreEventsThanNSinceT(n,t) {
-    // return whether we know for sure that there were more events than n since time t
-    var time_now = new Date().getTime();
-    var since_in_seconds = (time_now - t)/1000;
-    for(var i=0;i<this.time_slots.length;i++) {
-        if( since_in_seconds > this.time_slots[i] ) {
-        {
-            return n_since[i] > n;
+    // return true if we think there may have been more events than n since time t (in seconds since 1970-01-01)
+    var time_now = getTime();
+    this.refreshSlots(time_now);
+    var seconds_since_t = time_now - t;
+    var events_found = this.n_since[0];
+    for( var i = 1; i < this.time_slots.length; i++ ) {
+        if( this.time_slots[i] > seconds_since_t ) {
+            break;
+        }
+        events_found += n_since[i];
+    }
+    return events_found > n;
+}
+
+UpdateLog.prototype.refreshSlots(time_now) {
+    for( var i = 0; i < this.time_slots.length; i++ ) {
+        var seconds_since_last_emptied = time_now - this.last_emptied[i];
+        if( seconds_since_last_emptied > this.time_slots[i] ) {
+            // move all the contents of this slot into the next one
+            this.n_since[i+1] += this.n_since[i];
+            this.n_since[i] = 0;
+            this.last_emptied[i] = time_now;
         }
     }
-    return false;
+}
+
+UpdateLog.prototype.getTime = function() {
+    // get the time, in seconds since 1970-01-01
+    return new Date().getTime() / 1000;
 }
